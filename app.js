b4761a32bd00ec655710b2f48d54da86e3e71e3a
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2022-02-21 10:44:32 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+const fs = require('fs');
+
+const myInterface = readline.createInterface({
+  input: fs.createReadStream('shoppingList.txt')
+});
+
+const printData = (data) => {
+  console.log(`Item: ${data}`);
+};
+
